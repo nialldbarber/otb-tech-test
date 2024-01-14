7707b7ccf7398e7b76d7006ea6ab409b8653340a
Niall Barber <niall.barber@olioex.com>
2024-01-14 12:13:45 +0000

    test: added automation tests

--- tests/app.spec.ts
+++ tests/app.spec.ts
@@ -1,18 +1,113 @@
-import { test, expect } from '@playwright/test';
+import { expect, test } from "@playwright/test";
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+test("that `price` is the default sort value", async ({ page }) => {
+	await page.goto("/");
+	await expect(page.getByLabel("filter-price")).toHaveAttribute(
+		"aria-pressed",
+		"true",
+	);
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test("when the alphabetical filter item is clicked, the list changes to reflect that", async ({
+	page,
+}) => {
+	await page.goto("/");
+	const initialOrder = await page.$$eval("[role='contentinfo']", (items) =>
+		items.map((item) => item.textContent),
+	);
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+	page.on("console", () => console.log(initialOrder));
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+	await page.click(".filter-alphabetically");
+	await expect(page.getByLabel("filter-alphabetically")).toHaveAttribute(
+		"aria-pressed",
+		"true",
+	);
+	await expect(page.getByLabel("filter-price")).toHaveAttribute(
+		"aria-pressed",
+		"false",
+	);
+	await expect(page.getByLabel("filter-rating")).toHaveAttribute(
+		"aria-pressed",
+		"false",
+	);
+	const newOrder = await page.$$eval("[role='contentinfo']", (items) =>
+		items.map((item) => item.textContent),
+	);
+
+	page.on("console", () => console.log(newOrder));
+
+	const alphabeticallySortedOrder = [...newOrder].sort();
+	expect(newOrder).toEqual(alphabeticallySortedOrder);
+	expect(initialOrder).not.toEqual(newOrder);
+});
+
+test("when the ratings is clicked, the list changes to reflect that", async ({
+	page,
+}) => {
+	await page.goto("/");
+	const initialOrder = await page.$$eval("[role='contentinfo']", (items) =>
+		items.map((item) => item.textContent),
+	);
+
+	await page.click(".filter-rating");
+
+	await expect(page.getByLabel("filter-rating")).toHaveAttribute(
+		"aria-pressed",
+		"true",
+	);
+	await expect(page.getByLabel("filter-price")).toHaveAttribute(
+		"aria-pressed",
+		"false",
+	);
+	await expect(page.getByLabel("filter-alphabetically")).toHaveAttribute(
+		"aria-pressed",
+		"false",
+	);
+	const newOrder = await page.$$eval("[role='contentinfo']", (items) =>
+		items.map((item) => item.textContent),
+	);
+	const ratingsSortedOrder = [...newOrder].sort((a, b) => {
+		if (a === null || b === null) {
+			return 0;
+		}
+		return Number(a) - Number(b);
+	});
+	expect(newOrder).toEqual(ratingsSortedOrder);
+	expect(initialOrder).not.toEqual(newOrder);
+});
+
+test("when the price filter item is clicked, the list changes to reflect that", async ({
+	page,
+}) => {
+	await page.goto("/");
+	const initialOrder = await page.$$eval("[role='contentinfo']", (items) =>
+		items.map((item) => item.textContent),
+	);
+
+	await page.click(".filter-price");
+	await expect(page.getByLabel("filter-price")).toHaveAttribute(
+		"aria-pressed",
+		"true",
+	);
+	await expect(page.getByLabel("filter-rating")).toHaveAttribute(
+		"aria-pressed",
+		"false",
+	);
+	await expect(page.getByLabel("filter-alphabetically")).toHaveAttribute(
+		"aria-pressed",
+		"false",
+	);
+	const newOrder = await page.$$eval("[role='contentinfo']", (items) =>
+		items.map((item) => item.textContent),
+	);
+
+	const priceSortedOrder = [...newOrder].sort((a, b) => {
+		if (a === null || b === null) {
+			return 0;
+		}
+		return Number(b) - Number(a);
+	});
+	expect(newOrder).toEqual(priceSortedOrder);
+	expect(initialOrder).toEqual(newOrder);
 });
